1eb230a80f94f40bc51e062b1d1c601acf9091ac
Andreas Steffan <zycrophat@googlemail.com>
2025-10-13 22:16:57 +0200

    added background service

---
+++ MessageTrigger/Common/MessageConsumerBackgroundService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MessageTrigger.Common
+{
+    internal class MessageConsumerBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MessageConsumerBackgroundService> logger;
+        private readonly IMessageConsumer messageConsumer;
+
+        public MessageConsumerBackgroundService(
+            ILogger<MessageConsumerBackgroundService> logger,
+            IMessageConsumer messageConsumer)
+        {
+            ArgumentNullException.ThrowIfNull(logger);
+            ArgumentNullException.ThrowIfNull(messageConsumer);
+            this.logger = logger;
+            this.messageConsumer = messageConsumer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    logger.LogInformation("Starting message consumption");
+                    await messageConsumer.ConsumeAsync(stoppingToken);
+                }
+                catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+                {
+                    // Graceful shutdown
+                    logger.LogInformation("Message consumption cancelled");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogWarning(ex, "Error in message consumption");
+                }
+            }
+        }
+    }
+}
